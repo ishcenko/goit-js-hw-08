3c90ed20863eb7244d67485535635e73126ca1ec
ishcenko <107360109+ishcenko@users.noreply.github.com>
2023-05-02 10:34:40 +0300

    код для 3 завдання

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle'
 
+const feedbackForms = document.querySelector('.feedback-form');
+const emailInputForm = document.querySelector('input');
+const messageTextArea = document.querySelector('textarea');
+
+const KEY_LOCALSTORAGE = "feedback-form-state";
+
+function inputText(e) {
+    const formDataValue = {
+        email: emailInputForm.value,
+        message: messageTextArea.value
+    };
+    localStorage.setItem(KEY_LOCALSTORAGE, JSON.stringify(formDataValue));
+};
+
+const onThrottleInput = throttle(inputText, 500);
+
+emailInputForm.addEventListener('input', onThrottleInput);
+messageTextArea.addEventListener('input', onThrottleInput);
+feedbackForms.addEventListener('submit', sendCompletedForm);
+
+enterTextInBox();
+
+function enterTextInBox() {
+    const saveValue = JSON.parse(localStorage.setItem(KEY_LOCALSTORAGE, JSON.stringify(formDataValue)));
+    if (saveValue) {
+        emailInputForm.value = saveValue.email;
+        messageTextArea.value = saveValue.message;
+    };
+};
+
+function sendCompletedForm(e) {
+    e.preventDefault();
+    const emailText = emailInputForm.value;
+    const messageText = messageTextArea.value;
+    
+    if (emailText !== '' && messageText !== '') {
+        const formDataValue = {
+            email: emailText,
+            message: messageText
+        };
+        console.log(formDataValue);
+        feedbackForms.reset();
+    } else {
+        alert('Будь ласка, заповніть всі поля форми')
+    };
+    localStorage.removeItem(KEY_LOCALSTORAGE);
+};
